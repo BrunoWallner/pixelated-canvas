62d9210df6d9f9e914318f1d4d0be73efc52f55c
luca <b.lucab1211@gmail.com>
2021-02-27 16:01:46 +0100

    replaced glium with sdl2 as backend

--- src/engine.rs
+++ src/engine.rs
@@ -1,130 +1,29 @@
-use glium::glutin;
-use glium::Surface;
-use glium::implement_vertex;
-use glium::uniform;
 
-#[derive(Copy, Clone)]
-struct Vertex {
-    position: [f32; 2],
-}
-implement_vertex!(Vertex, position);
-
-/*
-fn main() {
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
-
-    //shaders
-    let vertex_shader_src = r#"
-        #version 140
-
-        in vec2 position;
-        out vec2 attr;
-
-        uniform mat4 matrix;
-
-        void main() {
-            attr = position;
-            gl_Position = matrix * vec4(position, 0.0, 1.0);
-        }
-    "#;
-
-    let fragment_shader_src = r#"
-    #version 140
-
-    in vec2 attr;
-    out vec4 color;
-
-    void main() {
-        color = vec4(attr, attr[0] * attr[1], 1.0);
-    }
-    "#;
-
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
-    let mut animation: f32 = -0.5;
-
-    event_loop.run(move |ev, _, control_flow| {
-        animation +=0.0001;
-        if animation > 0.5 { animation = -0.5; }
-        let uniforms = uniform! {
-            matrix: [
-                [1.0, 0.0, 0.0, 0.0], //rotation
-                [0.0, 1.0, 0.0, 0.0], //scale
-                [0.0, 0.0, 0.5, 0.0], //IDK
-                [animation, animation, 0.0, 1.0f32], //position
-            ]
-        };
-
-        let vertex1 = Vertex { position: [-0.5, -0.5] };
-        let vertex2 = Vertex { position: [ 0.0,  0.5] };
-        let vertex3 = Vertex { position: [ 0.5, -0.25] };
-        let shape = vec![vertex1, vertex2, vertex3];
-
-        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-
-        let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
-
-        target.draw(&vertex_buffer, &indices, &program, &uniforms,
-            &Default::default()).unwrap();
-
-        target.finish().unwrap();
-
-        match ev {
-            glutin::event::Event::WindowEvent { event, .. } => match event {
-                glutin::event::WindowEvent::CloseRequested => {
-                    *control_flow = glutin::event_loop::ControlFlow::Exit;
-                    return;
-                },
-                _ => return,
-            },
-            _ => (),
-        }
-    });
-}
-*/
+use sdl2::pixels::Color;
+use sdl2::EventPump;
+use sdl2::keyboard::Keycode;
+use sdl2::render::Canvas;
 
 pub struct Window {
-    display: glium::Display,
-    framebuffer: Vec<Pixel>
+    sdl_context: sdl2::Sdl,
+    canvas: Canvas<sdl2::video::Window>
 }
-
-pub struct event_loop;
-impl event_loop {
-    pub fn new() -> glutin::event_loop::EventLoop<()> {
-        glutin::event_loop::EventLoop::new()
-    }
-}
-
 impl Window {
-    pub fn new(ev: glutin::event_loop::EventLoop<()>) -> Self {
-        let wb = glutin::window::WindowBuilder::new();
-        let cb = glutin::ContextBuilder::new();
+    pub fn new(size: [u32; 2], title: &str) -> Self {
+        let sdl_c = sdl2::init().unwrap();
+        let video_s= sdl_c.video().unwrap();
 
-        Window{ display: glium::Display::new(wb, cb, &ev).unwrap(), framebuffer: vec!() }
+        let win = video_s.window(title, size[0], size[1])
+        .position_centered()
+        .build()
+        .unwrap();
+
+        let can = win.into_canvas().build().unwrap();
+
+        Window{ sdl_context: sdl_c, canvas: can }
     }
 
-    pub fn get_display(&self) -> &glium::Display {
-        &self.display
+    pub fn event_pump(&self) -> sdl2::EventPump {
+        self.sdl_context.event_pump().unwrap()
     }
-
-    pub fn draw(&mut self, pixel: Pixel) {
-        self.framebuffer.push(pixel);
-    }
-
-    pub fn update(&self, display: &glium::Display) {
-        let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
-        target.finish().unwrap();
-    }
-}
-
-#[derive(Copy, Clone)]
-pub struct Pixel {
-    pub position: [f32; 2],
-    pub color: [f32; 4]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 mod engine;
 use engine::*;
 
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 fn main() {
-    let mut ev = event_loop::new();
-    let mut window = Window::new(ev);
-    let mut pixel = Pixel{ position: [0.25, 0.25], color: [1.0, 1.0, 1.0, 1.0] };
+    let mut window = Window::new([800, 800], "Test");
+
+    let mut event_pump = window.event_pump();
 
     //game loop
-    loop {
-        window.draw(pixel);
-        window.update(window.get_display());
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        
     }
 }
